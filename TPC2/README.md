96a7561720886e8360d7df2e8683fdcf18b380de
Migs082 <93313213+Migs082@users.noreply.github.com>
2024-02-25 20:06:52 +0000

    Update

---
+++ TPC2/README.md
@@ -0,0 +1,14 @@
+# TPC2: Mapa Virtual
+
+## Autor:
+- a100837
+- Miguel Ângelo Martins Guimarães
+
+## Resumo
+
+Neste trabalho, utilizou-se o material fornecido pelo docente, um ficheiro json que contém as informações sobre várias cidades de Portugal e informações sobre as ligações entre estas.
+
+Tarefas:
+- Criar uma página estática para cada cidade com nomes c1.html, c2.html, etc. Estas páginas devem conter todas as informações da cidade, incluindo as ligações que esta cidade possui.
+- Uma página principal em que todas as cidades se encontram listadas por ordem alfabetica.
+- Utilizar nodeJs para criar um servidor que permita visitar a página principal atráves de "/" e as restantes páginas das cidades atráves "/c2" "/c3" etc..
---
+++ TPC2/pageMaker.py
@@ -0,0 +1,106 @@
+""" 
+    Name: Miguel Guimarães
+    University: Universidade do Minho
+    Engenharia Web 2024 / Web Engeneering 2024 
+
+    Nota: 
+    - No intuito de otimizar o conteúdo das ligações, o JSON é percorrido apenas uma vez como um desafio de otimização.
+    - Todo o código está escrito em inglês por escolha.
+    - O ficheiro JSON deve ser fornecido através de um pipe (usar cat mapa-virtual.json | python3 pageMaker.py).
+"""
+
+# Imports
+import json
+import sys
+
+# Setup variables
+mapString = sys.stdin.read() # Read file path from stdin (cat mapa-virtual.json | python3 pageMaker.py)
+pagesFolder = "pags"
+
+############################### Aux functions ###############################
+# Returns the name of a city using its id
+def find_city_name (mapInfo, target_id):
+    for data in mapInfo['cidades']:
+        if data["id"] == target_id:
+            return data["nome"]
+
+############################### Code ###############################
+# Open json
+#maps = open(mapString, mode="r", encoding='utf-8')
+mapInfo = json.loads(mapString)
+
+############################### City connection Content ###############################
+cityConns = {} 
+
+# Iterate every connection
+for connection in mapInfo['ligacoes']:
+    conn_id = connection.get("id")
+    conn_origin = connection.get("origem")
+    conn_distance = connection.get("distância")
+    conn_destination = connection.get("destino")
+
+    destination_name = find_city_name(mapInfo, conn_origin)
+
+    # Save info of this connection in a dictionary
+    info = {}
+    info["id"] = conn_id
+    info["origem"] = conn_origin
+    info["distância"] = conn_distance
+    info["destino"] = conn_destination
+    info["nome_destino"] = destination_name
+
+    # Add the info of this connection  
+    if conn_origin in cityConns:
+        cityConns[conn_origin].append(info)  # Add to array if it exists
+    else:
+        cityConns[conn_origin] = [info]  # Create array if it doesn't exist
+
+############################### City Content ###############################
+# Iterate every city
+for data in mapInfo['cidades']:
+    contentHTML = f"""
+        <h1> Cidade "{data["nome"]}" </h1>
+        <br> <b> Id: </b> {data["id"]} </br>
+        <br> <b> Distrito: </b> {data["distrito"]} </br>
+        <br> <b> População: </b> {data["população"]} </br>
+        <br> <b> Descrição: </b> {data["descrição"]} </br>
+        <b> Ligações: </b> 
+    """
+
+    # Add all connections of this city
+    for conn in cityConns.get(data["id"], []):
+        c_id = conn.get("id")
+        c_origem = conn.get("origem")
+        c_distance = conn.get("distância")
+        c_destination= conn.get("destino")
+        c_city_name = find_city_name(mapInfo, c_destination)
+
+        contentHTML += f"""
+            <br> <b> Ligação id# {c_id} ({data["nome"]} - {c_city_name}): </b> distância {c_distance} </br>             
+        """
+    ############################### HTML ###############################
+    preHTML = f"""
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <title> {data["nome"]} </title>
+                <link rel="stylesheet" href="w3.css">
+                <meta charset="UTF-8">
+            </head>
+        <body>
+    """
+
+    postHTML = "</body></html>"
+    
+    ############################### SAVING ###############################
+    # Save the page
+    pageFolder = pagesFolder + "/" + data["id"] + ".html"
+    page = open(pageFolder, mode="w", encoding='utf-8')
+
+    finalContent = preHTML + contentHTML + postHTML
+
+    page.write(finalContent)
+    page.close()
+
+# Ending
+print("Finished!")
